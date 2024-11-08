d2416de5610989e1bdaa0b7c56684f35ece6880e
Nimisha Wilson <nimishawilson123@gmail.com>
2024-11-08 17:03:40 +0530

    UI for signin page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { RegistrationComponent } from './registration/registration.component';
+import { SignInComponent } from './sign-in/sign-in.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RegistrationComponent],
+  imports: [RouterOutlet, RegistrationComponent, SignInComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
