b2b83a665cecf18d5b53302c5a689a2e963eba96
Nimisha Wilson <nimishawilson123@gmail.com>
2024-11-08 17:11:28 +0530

    create UI for forgot password page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { RegistrationComponent } from './registration/registration.component';
 import { SignInComponent } from './sign-in/sign-in.component';
+import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RegistrationComponent, SignInComponent],
+  imports: [RouterOutlet, RegistrationComponent, SignInComponent, ForgotPasswordComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
